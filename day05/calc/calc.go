2e574b0afdb70b45fbc8ce7122a7b274290ebb65
root <wgqbeam@hnu.edu.cn>
2022-01-02 14:21:51 +0800

    package

--- day05/calc/calc.go
+++ day05/calc/calc.go
@@ -1,5 +1,11 @@
 package calc
 
+import "fmt"
+
+func init() {
+	fmt.Println("import本包时，自动执行.")
+}
+
 func Add(x, y int) int {
 	return x + y
 }
--- day05/calc_m/xxx.go
+++ day05/calc_m/xxx.go
@@ -1,11 +1,15 @@
 package main
 
 import (
-	"beamwang/day05/calc"
+	cc "beamwang/day05/calc"
 	"fmt"
 )
 
+func init() {
+	fmt.Println("自动执行.")
+}
+
 func main() {
-	a := calc.Add(1, 3)
+	a := cc.Add(1, 3)
 	fmt.Println(a)
 }
